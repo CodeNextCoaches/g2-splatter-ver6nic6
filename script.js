ec7649bed41695f5607688e5682367592ce76800
Student <student@Admins-MacBook-Air-6.local>
2018-02-17 14:18:59 -0500

    add splatter

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 // declare and initialize global variables
 
+var canvas;
+var canvasWidth = 500;
+var canvasHeight = 500;
+var button;
+var spread;
+var slider;
+
+function setup() {
+
+  canvas = createCanvas(canvasWidth,canvasHeight);
+  background(219,112,147);
+
+  canvas.parent("canvas-area");
+  
+  canvas.mousePressed(drawEllipse);
+  
+  button = select("#clear-button");
+  button.mousePressed(setup);
+  
+  slider = select("#slider");
+  slider.input(updateSpread);
+  updateSpread();
+
+}
+ 
+function drawEllipse() {
+  
+  fill("#ffffff");
+  ellipse(mouseX,mouseY,spread,spread);
+
+}
+
+function updateSpread () {
+
+  spread=slider.value();
+
+}
+
 /*
- * setup()
- */
-
-
-/*
- * drawEllipse()
- */
-
-
-/*
- * updateSpread()
  * This function simply sets the spread variable to the value of the slider.
  * For example, if the slider is at 75, the spread will be set to 75 as well.
  */
 
+drawSplatter() {
+  random(10,15);
+  fill(random(0,255),random(0,255),random(0,255));
+  noStroke();
+  for(i=0,i<)
+
+}
 
  /*
-  * drawSplatter()
   * Creates ellipses on the canvas around where the mouse is clicked.
   * Details:
   * -The function creates between 10 and 14 ellipses (random).
